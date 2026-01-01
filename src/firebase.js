ba035e8215647a2bdf13e07018606b7772454df3
Ishan Hatharasinghe <ishanhatharasinghe222@gmail.com>
2026-01-01 15:56:09 +0530

    Update firebase.js

--- src/firebase.js
+++ src/firebase.js
@@ -5,14 +5,14 @@ import { getFirestore } from "firebase/firestore";
 
 // Firebase config
 const firebaseConfig = {
-  apiKey: "AIzaSyARWCfQFeHb-4OKjJIoeP6oeMajx1nHhkE",
-  authDomain: "ridelink-26c32.firebaseapp.com",
-  databaseURL: "https://ridelink-26c32-default-rtdb.firebaseio.com", // ✅ Ensure correct RTDB URL
-  projectId: "ridelink-26c32",
-  storageBucket: "ridelink-26c32.appspot.com",
-  messagingSenderId: "296583693513",
-  appId: "1:296583693513:web:3dcf1a741cab1dc22af918",
-  measurementId: "G-5STSMT9RWJ"
+  apiKey: "",
+  authDomain: "",
+  databaseURL: "", 
+  projectId: "",
+  storageBucket: "",
+  messagingSenderId: "",
+  appId: "",
+  measurementId: ""
 };
 
 // Initialize Firebase
